8fc0749a2dc4f2299f3edb4d6a87b08930601fae
ZFJ <q771307617@gmail.com>
2017-12-13 20:37:42 +0800

    feat:"路由样式"

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -19,15 +19,16 @@ export default {
     window.$('.ddd').text('aaaa');
     var map;
     var zoom = 11;
-    var img, img1;
+    // var img;
+    var img1;
     map = new window.T.Map('mapDiv');
     map.centerAndZoom(new window.T.LngLat(119.286105, 29.470681), zoom);
     var bd = new window.T.LngLatBounds(
       new window.T.LngLat(118.8940912546568, 29.20439576265778),
       new window.T.LngLat(120.32081607375262, 30.155545642054992));
-    img = new window.T.ImageOverlay('http://1.img.dianjiangla.com/jdAssets/jdCity.png', bd, {opacity: '.3', alt: '建德市'}, {minZoom: '1', maxZoom: '13'});
-    img1 = new window.T.ImageOverlay('http://1.img.dianjiangla.com/jdAssets/jdQh.png', bd, {opacity: '1', alt: '建德市'}, {minZoom: '13', maxZoom: '20'});
-    map.addOverLay(img);
+    // img = new window.T.ImageOverlay('http://1.img.dianjiangla.com/jdAssets/jdCity.png', bd, {opacity: '.3', alt: '建德市'}, {minZoom: '1', maxZoom: '13'});
+    img1 = new window.T.ImageOverlay('http://chuantu.biz/t6/173/1512980653x-1404793565.png', bd, {opacity: '.5', alt: '建德市'}, {minZoom: '13', maxZoom: '20'});
+    // map.addOverLay(img);
     map.addOverLay(img1);
   }
 };
