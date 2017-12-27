189037eb4453a22bc72854a1c8ea2a07a36bc7d8
ZFJ <q771307617@gmail.com>
2017-12-27 20:15:01 +0800

    feat:"api"

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,8 +26,8 @@ module.exports = {
     '@nuxtjs/proxy'
   ],
   proxy: [
-    ['/api', { target: 'http://dev.jd.yumc.pw' }],
-    ['/upload', { target: 'http://172.30.34.241:8081' }],
+    ['/api', { target: 'http://192.168.2.205' }],
+    ['/upload', { target: 'http://192.168.2.205:8181' }],
   ],
   // proxy: {
   //   '/api': {
