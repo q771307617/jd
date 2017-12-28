d3f7757fa8f9f51cba245d855a11df9df2c0dada
wsl <395548460@qq.com>
2017-12-28 09:58:23 +0800

    feat : 修改配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,10 +12,10 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
-    script:[
-      {src:'http://api.tianditu.com/api?v=4.0'},
-      {src:'http://lbs.tianditu.com/api/js4.0/opensource/openlibrary/ImageOverlay.js'},
-      {src: 'http://code.jquery.com/jquery-1.7.2.min.js' },
+    script: [
+      { src: 'http://api.tianditu.com/api?v=4.0' },
+      { src: 'http://lbs.tianditu.com/api/js4.0/opensource/openlibrary/ImageOverlay.js' },
+      { src: 'http://code.jquery.com/jquery-1.7.2.min.js' }
     ]
   },
   /*
@@ -26,18 +26,9 @@ module.exports = {
     '@nuxtjs/proxy'
   ],
   proxy: [
-    ['/api', { target: 'http://online.jd.yumc.pw/' }],
-    ['/upload', { target: 'http://online.jd.yumc.pw:8181' }],
+    ['/api', { target: 'http://dev.jd.yumc.pw' }],
+    ['/upload', { target: 'http://172.30.34.241:8081' }]
   ],
-  // proxy: {
-  //   '/api': {
-  //     target: 'http://dev.jd.yumc.pw',
-  //     // changeOrigin: true,
-  //     pathRewrite: {
-  //       '^/api': ''
-  //     }  
-  //   }
-  // },
   loading: { color: '#3B8070' },
   /*
   **页面过渡动画
@@ -45,7 +36,7 @@ module.exports = {
   css: [
     'assets/main.css',
     'assets/scss/reset.scss',
-    'element-ui/lib/theme-chalk/index.css',
+    'element-ui/lib/theme-chalk/index.css'
   ],
   /*
   **动态路由数组
@@ -54,47 +45,47 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    analyze: true,
+    // analyze: true,
     // or
     analyze: {
       analyzerMode: 'static'
-  },
+    },
     /*
     ** Run ESLint on save
     */
-    /*请求数据*/ 
+    /* 请求数据 */
     vendor: ['axios', 'element-ui'],
-    extend (config, ctx) {
+    extend(config, ctx) {
       if (ctx.dev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/
-        })
+        });
       }
       // 添加sass
-      const sassResourcesLoader = {  
-        loader: 'sass-resources-loader',  
-        options: {  
-          resources: [  
-           'assets/scss/reset.scss'
-          ]  
-        }  
-      }
-      // 遍历nuxt定义的loader配置，向里面添加新的配置。  
-      config.module.rules.forEach((rule) => {  
-        if (rule.test.toString() === '/\\.vue$/') {  
-          rule.options.loaders.sass.push(sassResourcesLoader)  
-          rule.options.loaders.scss.push(sassResourcesLoader)  
-        }  
-        if (['/\\.sass$/', '/\\.scss$/'].indexOf(rule.test.toString()) !== -1) {  
-          rule.use.push(sassResourcesLoader)
-        }  
-      })   
+      const sassResourcesLoader = {
+        loader: 'sass-resources-loader',
+        options: {
+          resources: [
+            'assets/scss/reset.scss'
+          ]
+        }
+      };
+      // 遍历nuxt定义的loader配置， 向里面添加新的配置
+      config.module.rules.forEach((rule) => {
+        if (rule.test.toString() === '/\\.vue$/') {
+          rule.options.loaders.sass.push(sassResourcesLoader);
+          rule.options.loaders.scss.push(sassResourcesLoader);
+        }
+        if (['/\\.sass$/', '/\\.scss$/'].indexOf(rule.test.toString()) !== -1) {
+          rule.use.push(sassResourcesLoader);
+        }
+      });
     }
   },
   plugins: [
     { src: '~plugins/element-ui', ssr: true }
   ]
-}
+};
